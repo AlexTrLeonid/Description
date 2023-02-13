0d1b0e25d6cdba23065e4a4d53abba5f8984b0d0
Alexandr Troshin <alexandr-troshin@mail.ru>
2023-02-13 22:54:34 +0400

    Домашняя работа  Задача №4

--- sixth/Program.cs
+++ sixth/Program.cs
@@ -5,16 +5,11 @@
 // 22 3 9 -> 22
 
 Console.WriteLine("Введите первое число: ");
-int firstNumber = int.Parse(Console.ReadLine());
-
-int firstNumber = Convert.ToInt32(Console.ReadLine()!);
+int firstNumber = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите второе число: ");
-int secondNumber = int.Parse(Console.ReadLine());
-
-int secondNumber = Convert.ToInt32(Console.ReadLine()!);
+int secondNumber = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите третье число: ");
-int thirdNumber = int.Parse(Console.ReadLine());
-int thirdNumber = Convert.ToInt32(Console.ReadLine()!);
+int thirdNumber = int.Parse(Console.ReadLine()!);
 if (firstNumber > secondNumber)
 {
     if (firstNumber > thirdNumber)
