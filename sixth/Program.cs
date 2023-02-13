7ce803c148f55135e12cecebeccc99dba083e816
AlexandrT <123582688+AlexTrLeonid@users.noreply.github.com>
2023-02-13 22:17:25 +0400

    int.Parse

--- sixth/Program.cs
+++ sixth/Program.cs
@@ -5,13 +5,13 @@
 // 22 3 9 -> 22
 
 Console.WriteLine("Введите первое число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+int firstNumber = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Введите второе число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Введите третье число: ");
-int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int thirdNumber = int.Parse(Console.ReadLine());
 if (firstNumber > secondNumber)
 {
     if (firstNumber > thirdNumber)
